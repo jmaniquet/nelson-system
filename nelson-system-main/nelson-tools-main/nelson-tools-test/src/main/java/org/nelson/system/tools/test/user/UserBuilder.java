5c1b8698d5e0765b75974a45fe5f52c65ab78c03
jmaniquet <jmaniquet@norsys.fr>
2015-07-18 15:19:16 +0200

    Basic user class with builder for tools-test module

---
+++ nelson-system-main/nelson-tools-main/nelson-tools-test/src/main/java/org/nelson/system/tools/test/user/UserBuilder.java
@@ -0,0 +1,51 @@
+package org.nelson.system.tools.test.user;
+
+import org.joda.time.DateTime;
+
+public class UserBuilder {
+
+	private Long id;
+	
+	private String name;
+	
+	private String givenName;
+	
+	private DateTime birthDate;
+	
+	private UserBuilder() {
+		super();
+	}
+	
+	public static UserBuilder builder() {
+		return new UserBuilder();
+	}
+	
+	public UserBuilder id(long pId) {
+		this.id = pId;
+		return this;
+	}
+	
+	public UserBuilder name(String pName) {
+		this.name = pName;
+		return this;
+	}
+	
+	public UserBuilder givenName(String pGivenName) {
+		this.givenName = pGivenName;
+		return this;
+	}
+
+	public UserBuilder birthDate(DateTime pBirthDate) {
+		this.birthDate = pBirthDate;
+		return this;
+	}
+	
+	public User build() {
+		User user = new User();
+		user.setId(this.id);
+		user.setName(this.name);
+		user.setGivenName(this.givenName);
+		user.setBirthDate(this.birthDate);
+		return user;
+	}
+}
