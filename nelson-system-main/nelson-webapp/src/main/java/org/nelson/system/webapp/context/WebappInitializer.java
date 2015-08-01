e77ee87a38d02f90ea95fe7af553e2af78146782
jmaniquet <jmaniquet@norsys.fr>
2015-08-01 14:44:35 +0200

    Basic webapp spring context with webflow-jsf integration, with custom path for flows and facelets

---
+++ nelson-system-main/nelson-webapp/src/main/java/org/nelson/system/webapp/context/WebappInitializer.java
@@ -0,0 +1,37 @@
+package org.nelson.system.webapp.context;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.nelson.system.core.api.context.CoreApiContext;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebappInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[] { CoreApiContext.class,
+								WebappContext.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/app/*" };
+	}
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		
+		// Use JSF view templates saved as *.xhtml, for use with Facelets
+		servletContext.setInitParameter("javax.faces.DEFAULT_SUFFIX", ".xhtml");
+		servletContext.setInitParameter("javax.faces.INTERPRET_EMPTY_STRING_SUBMITTED_VALUES_AS_NULL", "true");
+
+		// Let the DispatcherServlet be registered
+		super.onStartup(servletContext);
+	}
+}
