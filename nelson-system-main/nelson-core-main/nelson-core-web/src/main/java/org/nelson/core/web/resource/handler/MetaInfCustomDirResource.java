440b9ea8c8e8b6a4642220baa05abec84155deaa
jmaniquet <jmaniquet@norsys.fr>
2015-08-01 13:19:27 +0200

    Jsf tuning to resolver facelets from jar, in a the directory 'views' not under META-INF/resources

---
+++ nelson-system-main/nelson-core-main/nelson-core-web/src/main/java/org/nelson/core/web/resource/handler/MetaInfCustomDirResource.java
@@ -0,0 +1,26 @@
+package org.nelson.core.web.resource.handler;
+
+import java.net.URL;
+
+import javax.faces.application.ViewResource;
+
+public class MetaInfCustomDirResource extends ViewResource {
+
+	private static final String META_INF = "/META-INF";
+	private String metaInfCustomDir;
+	private String resourcePath;
+	
+	public MetaInfCustomDirResource(String metaInfCustomDir, String metaInfPath) {
+		super();
+		this.metaInfCustomDir = metaInfCustomDir;
+		this.resourcePath = metaInfPath;
+	}
+
+	@Override
+	public URL getURL() {
+		String metaInfPath = META_INF + metaInfCustomDir + resourcePath;
+		URL url = getClass().getResource(metaInfPath); // Resolves from JAR.
+		return url;
+	}
+
+}
---
+++ nelson-system-main/nelson-core-main/nelson-core-web/src/test/java/org/nelson/core/web/resource/AbstractMetaInfCustomDirResourceTest.java
@@ -0,0 +1,27 @@
+package org.nelson.core.web.resource;
+
+import java.net.URL;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public abstract class AbstractMetaInfCustomDirResourceTest {
+
+	protected static final String META_INF_ROOT = "/META-INF";
+	protected static final String TEST_CUSTOM_DIR = "/views";
+	protected static final String TEST_PATH = "/fake-common/fake-footer.xhtml";
+	
+	@Test
+	public abstract void testResourceFound();
+	
+	@Test
+	public abstract void testResourceNotFound();
+	
+	protected void verifyResultUrl(URL url) {
+		Assert.assertNotNull(url);
+		
+		String expectedResultString = META_INF_ROOT + TEST_CUSTOM_DIR + TEST_PATH;
+		String urlAsStr = url.toString();
+		Assert.assertTrue(urlAsStr.endsWith(expectedResultString));
+	}
+}
