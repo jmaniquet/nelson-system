a5fdcf2b6b4de16c26e6665fd7e588f8ad209af9
jmaniquet <jmaniquet@norsys.fr>
2015-08-06 19:27:23 +0200

    Reworking of home page to display buttons for each domain

--- nelson-system-main/nelson-webapp/src/main/java/org/nelson/system/webapp/context/WebappContext.java
+++ nelson-system-main/nelson-webapp/src/main/java/org/nelson/system/webapp/context/WebappContext.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.nelson.system.core.api.basenames.MessageLocationProvider;
+import org.nelson.system.core.api.basenames.MessageLocationProviderDefaultImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -76,6 +77,11 @@ public class WebappContext extends AbstractFacesFlowConfiguration {
 		JsfFlowHandlerAdapter a = new JsfFlowHandlerAdapter();
 		a.setFlowExecutor(flowExecutor());
 		return a;
+	}
+	
+	@Bean
+	public MessageLocationProvider webappMessageLocationProvider() {
+		return new MessageLocationProviderDefaultImpl("classpath:messages/home");
 	}
 	
 	@Bean
