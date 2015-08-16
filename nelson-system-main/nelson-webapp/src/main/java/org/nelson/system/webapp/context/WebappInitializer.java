fa4ed2cffd3a4ffeba18ad6fd31bd8d8ac5ffa35
jmaniquet <jmaniquet@norsys.fr>
2015-08-16 12:41:35 +0200

    Moving low level db access component to a dedicated module (mainly mybatis generated stuff)

--- nelson-system-main/nelson-webapp/src/main/java/org/nelson/system/webapp/context/WebappInitializer.java
+++ nelson-system-main/nelson-webapp/src/main/java/org/nelson/system/webapp/context/WebappInitializer.java
@@ -3,7 +3,7 @@ package org.nelson.system.webapp.context;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
-import org.nelson.system.core.api.context.CoreApiContext;
+import org.nelson.system.core.db.context.CoreDbContext;
 import org.nelson.system.core.web.context.CoreWebContext;
 import org.nelson.system.infrastructure.environment.context.InfrastructureEnvironmentContext;
 import org.nelson.system.personne.web.context.PersonneWebContext;
@@ -14,7 +14,7 @@ public class WebappInitializer extends AbstractAnnotationConfigDispatcherServlet
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
 		return new Class<?>[] { InfrastructureEnvironmentContext.class,
-								CoreApiContext.class,
+								CoreDbContext.class,
 								CoreWebContext.class,
 								PersonneWebContext.class,
 								WebappContext.class };
