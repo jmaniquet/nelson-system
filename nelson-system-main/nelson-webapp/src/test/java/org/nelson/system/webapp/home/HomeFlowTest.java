00ca0ff875e5614c4f5f3f9bfec33c137d22a21a
jmaniquet <j15ic00@FX041411S.ad.si2m.tec>
2016-01-29 14:36:30 +0100

    Closing main page should not throw some server error

--- nelson-system-main/nelson-webapp/src/test/java/org/nelson/system/webapp/home/HomeFlowTest.java
+++ nelson-system-main/nelson-webapp/src/test/java/org/nelson/system/webapp/home/HomeFlowTest.java
@@ -2,8 +2,12 @@ package org.nelson.system.webapp.home;
 
 import org.junit.Test;
 import org.nelson.system.core.test.web.flow.AbstractNelsonXmlFlowExecutionTests;
+import org.nelson.system.core.test.web.flow.MockExtCtxBuilder;
+import org.nelson.system.core.web.flow.events.CommonEvent;
+import org.nelson.system.core.web.flow.states.ClosedState;
 import org.springframework.webflow.config.FlowDefinitionResource;
 import org.springframework.webflow.config.FlowDefinitionResourceFactory;
+import org.springframework.webflow.test.MockExternalContext;
 
 public class HomeFlowTest extends AbstractNelsonXmlFlowExecutionTests {
 	
@@ -16,5 +20,18 @@ public class HomeFlowTest extends AbstractNelsonXmlFlowExecutionTests {
 	public void testStartFlow() {
 		startFlow();
 		assertCurrentStateEquals("home");
+	}
+	
+	@Test
+	public void testHomeStateAndClose() {
+		setCurrentState("home");
+		MockExternalContext context = MockExtCtxBuilder
+				.builder()
+				.withEvent(CommonEvent.CLOSE)
+				.build();
+		
+		resumeFlow(context);
+		
+		assertFlowExecutionEndedWithOutcome(new ClosedState());
 	}
 }
